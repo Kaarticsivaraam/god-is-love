9187a57ade3f70f97f57e7d9a24c92935345fe01
Unique <Unique>
2016-09-08 00:51:02 +0530

    added file used to test capability of Object class.

---
+++ ObjectToInteger.java
@@ -0,0 +1,8 @@
+class ObjectToInteger{
+	public static void main(String[] arg){
+		ObjectToInteger test = new ObjectToInteger();
+		Integer intVal = (Integer)test.number;
+		System.out.print(test.number);
+	}
+	Object number = 8.0;
+}
