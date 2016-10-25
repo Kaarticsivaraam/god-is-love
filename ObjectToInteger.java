2152e0004cd8e0d72d9271a99ffc0a83e46aac1b
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2016-10-25 23:21:02 +0530

    removed unwanted files

--- ObjectToInteger.java
+++
@@ -1,8 +0,0 @@
-class ObjectToInteger{
-	public static void main(String[] arg){
-		ObjectToInteger test = new ObjectToInteger();
-		Integer intVal = (Integer)test.number;
-		System.out.print(test.number);
-	}
-	Object number = 8.0;
-}
